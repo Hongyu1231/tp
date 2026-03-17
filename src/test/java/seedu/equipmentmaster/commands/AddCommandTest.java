43f6d23e9ef8491f9188f3eb570840935b414598
wjw55 <wjw50100@gmail.com>
2026-03-17 23:06:36 +0800

    Update AddCommandTest to test new execute function

--- src/test/java/seedu/equipmentmaster/commands/AddCommandTest.java
+++ src/test/java/seedu/equipmentmaster/commands/AddCommandTest.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.Test;
 
 import seedu.equipmentmaster.equipment.Equipment;
 import seedu.equipmentmaster.equipmentlist.EquipmentList;
+import seedu.equipmentmaster.exception.EquipmentMasterException;
+import seedu.equipmentmaster.semester.AcademicSemester;
 import seedu.equipmentmaster.storage.Storage;
 import seedu.equipmentmaster.ui.Ui;
 
@@ -13,19 +15,31 @@ public class AddCommandTest {
     private static final String TEST_FILE_PATH = "test_equipment.txt";
 
     @Test
-    public void execute_validEquipment_addsToList() {
+    public void execute_validEquipment_addsToList() throws EquipmentMasterException {
+
         EquipmentList equipments = new EquipmentList();
         Ui ui = new Ui();
         Storage storage = new Storage(TEST_FILE_PATH, ui);
 
-        AddCommand command = new AddCommand("STM32", 5);
+        // 1. Create dummy data for your new fields
+        AcademicSemester testSem = new AcademicSemester("AY2023/24 Sem1");
+        double testLifespan = 5.5;
+
+        // 2. Pass all 4 arguments into the AddCommand constructor
+        AddCommand command = new AddCommand("STM32", 5, testSem, testLifespan);
 
         command.execute(equipments, ui, storage);
 
+        // Verify the list size increased
         assertEquals(1, equipments.getSize());
 
+        // Verify the equipment was created with the correct data
         Equipment added = equipments.getEquipment(0);
         assertEquals("STM32", added.getName());
         assertEquals(5, added.getQuantity());
+
+        // 3. Assert the new fields are correct
+        assertEquals(testSem, added.getPurchaseSem());
+        assertEquals(testLifespan, added.getLifespanYears(), 0.0001);
     }
 }
